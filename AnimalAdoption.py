b97aa99f4bcec6af69f81b77c1869385c0810e1f
Pandaxyz <98690785+Pandaxyz-xd@users.noreply.github.com>
2022-07-17 16:39:04 +0200

    Add files via upload

---
+++ AnimalAdoption.py
@@ -0,0 +1,52 @@
+# Api: https://www.proxy-list.download/api/v1/
+# Website: https://www.proxy-list.download
+# Developed by @Pandaxyz
+
+import requests,time,random,os,json,webbrowser
+banner = """ 
+
+
+
+░█████╗░███╗░░██╗██╗███╗░░░███╗░█████╗░██╗░░░░░                ░█████╗░██████╗░░█████╗░██████╗░████████╗██╗░█████╗░███╗░░██╗
+██╔══██╗████╗░██║██║████╗░████║██╔══██╗██║░░░░░                ██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║
+███████║██╔██╗██║██║██╔████╔██║███████║██║░░░░░                ███████║██║░░██║██║░░██║██████╔╝░░░██║░░░██║██║░░██║██╔██╗██║
+██╔══██║██║╚████║██║██║╚██╔╝██║██╔══██║██║░░░░░                ██╔══██║██║░░██║██║░░██║██╔═══╝░░░░██║░░░██║██║░░██║██║╚████║
+██║░░██║██║░╚███║██║██║░╚═╝░██║██║░░██║███████╗                ██║░░██║██████╔╝╚█████╔╝██║░░░░░░░░██║░░░██║╚█████╔╝██║░╚███║
+╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚══════╝                ╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░░░░░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝
+"""
+menu = """ 
+
+          ╔══════════════╗
+          |   Animals    |
+          |--------------|
+          | [1] Dog      |
+          | [2] Cat      |
+          | [3] Fox      |
+          | [4] Bird     |
+          ╚══════════════╝
+
+"""
+os.system("cls")
+print(banner)
+print(menu)
+select = int(input("[>>] "))
+
+if select == 1:
+	dog_request = requests.get("https://some-random-api.ml/animal/dog")
+	dog_json = dog_request.json()
+	webbrowser.open(dog_json["image"])
+elif select == 2:
+	cat_request = requests.get("https://some-random-api.ml/animal/cat")
+	cat_json = cat_request.json()
+	webbrowser.open(cat_json["image"])
+elif select == 3:
+	fox_request = requests.get("https://some-random-api.ml/animal/fox")
+	fox_json = fox_request.json()
+	webbrowser.open(fox_json["image"])
+elif select == 4:
+	bird_request = requests.get("https://some-random-api.ml/animal/bird")
+	bird_json = bird_request.json()
+	webbrowser.open(bird_json["image"])
+
+
+
